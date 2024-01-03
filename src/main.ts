849ee83254f70877d561e178afb0fcf7d3afc055
xuyaqin <xuyaqin@gasfety.com>
2024-01-03 17:38:45 +0800

    feat: update src

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,14 @@
 import express from 'express';
 import Service from "./service/Service";
-import PGDatabase from "./entity/database/PGDatabase";
-import ResponseDto from "./entity/responseDto/ResponseDto";
 
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-const db = new PGDatabase();
-app.get('/connect', (req, res) => {
-    db.connect().then(response => {
-        res.json(response);
-    }).catch((err) => {
-        res.json(err);
-    })
-})
 // 注册所有的服务
 Service.register();
 // 加载所有的服务
 Service.load(app);
 // 监听
 app.listen(3000);
+console.log('【服务发布地址】http://localhost:3000')
